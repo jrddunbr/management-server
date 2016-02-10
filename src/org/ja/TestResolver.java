95f9543d5d3b0a0d87d7de110d87a5b6d09be502
Jared Dunbar <jrddunbr@gmail.com>
2016-02-09 19:06:47 -0500

    adding a bunch of stuff now. Removing TALOS dependency

---
+++ src/org/ja/TestResolver.java
@@ -0,0 +1,12 @@
+
+package org.ja;
+
+/**
+ *
+ * @author jared
+ */
+public class TestResolver {
+    public static void main(String[] args) {
+        System.out.println(ManagementServer.getServerNameFromIP("128.153.145.3"));
+    }
+}
